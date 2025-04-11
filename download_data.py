31520b972f79bd91c5ec273fcf3dd75e0bb2fbc6
ThijmenB <thijmen.broeren@gmail.com>
2025-04-11 23:56:24 +0200

    added extra data to download automatically

--- download_data.py
+++ download_data.py
@@ -8,9 +8,11 @@ import io
 from tqdm import tqdm
 import kagglehub
 import shutil
+import zipfile
+import gdown
 
 
-def download_img_from_url(folder: str, dataframe: pd.DataFrame, id_column: str, url_column: str):
+def download_img_from_url(folder: str, dataframe: pd.DataFrame, id_column: str, url_column: str) -> None:
     """
     Downloads images from a list of URL's.
 
@@ -19,25 +21,30 @@ def download_img_from_url(folder: str, dataframe: pd.DataFrame, id_column: str, 
     :param id_column: column that holds the unique identifier of the item, to be added to the filename
     :param url_column: column that holds the url information, which need to be opened and scraped
     """
+    # Create save folder if needed
     if not os.path.exists(folder):
         os.makedirs(folder)
 
+    # Initialize error counter
     count = 0
     for i, row in tqdm(dataframe.iterrows(), total=dataframe.shape[0], desc="Downloading image"):
+        # Set variables
         url = row[url_column][2:-2]
         filename = f'{row[id_column]}.jpg'
         file_path = os.path.join(folder, filename)
 
+        # Try to download image from url
         try:
             if not os.path.isfile(file_path):
+                # Download image from url
                 image_content = requests.get(url, timeout=10).content
                 image_file = io.BytesIO(image_content)
                 image = Image.open(image_file)
 
+                # Write contents on image
                 with open(file_path, "wb") as f:
                     image.save(f, "JPEG")
-
-        except Exception as e:
+        except Exception as e:  # Add one to error counter
             count += 1
 
     print(f"Failed downloading {count} images")
@@ -45,7 +52,10 @@ def download_img_from_url(folder: str, dataframe: pd.DataFrame, id_column: str, 
 
 def download_from_kaggle(kaggle_path: str, output_path: str) -> None:
     """
+    Downloads a dataset from kaggle and moves it toe the desired output path.
 
+    :param kaggle_path: name of kaggle dataset
+    :param output_path: path to output folder
     """
     # Create output path if it doesn't exist
     if not os.path.exists(output_path):
@@ -53,34 +63,152 @@ def download_from_kaggle(kaggle_path: str, output_path: str) -> None:
 
     # Download latest version
     path = kagglehub.dataset_download(kaggle_path)
-    shutil.move(path, output_path)
+
+    # os.listdir(path)
+    shutil.move(f"{path}/{os.listdir(path)[0]}", output_path)
 
 
-def main_download_data():
+def download_wider_face() -> None:
     """
-
+    Download the WIDER-face dataset from huggingface and places it in the desired folders.
     """
-    # df = pd.read_parquet("data/Embedding_On_The_Wall/full_df_clf_corrected.parquet")
-    # download_img_from_url("data/Embedding_On_The_Wall/full_images", df, "id", "img_link")
+    # Set the url's to download from
+    urls = [
+        "https://huggingface.co/datasets/CUHK-CSE/wider_face/resolve/ba0019b20b8c674d1f7bb8426f36ce69ea082648/data/WIDER_train.zip?download=true",
+        "https://huggingface.co/datasets/CUHK-CSE/wider_face/resolve/ba0019b20b8c674d1f7bb8426f36ce69ea082648/data/WIDER_val.zip?download=true",
+        "https://huggingface.co/datasets/CUHK-CSE/wider_face/resolve/ba0019b20b8c674d1f7bb8426f36ce69ea082648/data/WIDER_test.zip?download=true",
+        "https://huggingface.co/datasets/CUHK-CSE/wider_face/resolve/ba0019b20b8c674d1f7bb8426f36ce69ea082648/data/wider_face_split.zip?download=true"
+    ]
 
+    # Set the temporary save folders
+    save_folders_temp = [
+        "data_temp/WIDER_train",
+        "data_temp/WIDER_val",
+        "data_temp/WIDER_test",
+        "data_temp/wider_face_split"
+    ]
+
+    # Set the desired save folders
+    save_folders = [
+        "data/WIDER_face/train",
+        "data/WIDER_face/val",
+        "data/WIDER_face/test",
+        "data/WIDER_face/wider_face_split"
+    ]
+
+    # Iterate over every dataset from WIDER face
+    for url, temp_save_folder, save_folder in zip(urls, save_folders_temp, save_folders):
+        # Collect the data
+        response = requests.get(url, stream=True)
+        total_size = int(response.headers.get("Content-Length", 0))
+        chunk_size = 1024  # 1 KB
+
+        # Load chunks into memory
+        buffer = io.BytesIO()
+
+        # Download dataset
+        with tqdm(total=total_size, unit="B", unit_scale=True, desc=f"Downloading {temp_save_folder.split('/')[-1]}", ncols=100) as pbar:
+            for chunk in response.iter_content(chunk_size=chunk_size):
+                if chunk:  # filter out keep-alive chunks
+                    buffer.write(chunk)
+                    pbar.update(len(chunk))
+
+        # Go back to the start of the buffer
+        buffer.seek(0)
+
+        # Extract the downloaded zip file
+        print(f"Extracting {temp_save_folder.split('/')[-1]}...\n")
+        with zipfile.ZipFile(buffer) as z:
+            z.extractall(temp_save_folder)
+
+        # Set folder to move data from
+        src_folder = f"{temp_save_folder}/{temp_save_folder.split('/')[-1]}"
+
+        # Collect all file paths to move
+        all_files = []
+        for root, _, files in os.walk(src_folder):
+            for file in files:
+                file_path = f"{root}/{file}"
+                all_files.append(file_path)
+
+        # Move all files to the desired save folders
+        for file in tqdm(all_files, ncols=100, desc=f"Moving data from {temp_save_folder.split('/')[-1]}"):
+            # Create folders if needed
+            os.makedirs(os.path.dirname(f"{save_folder}{file[len(src_folder):]}"), exist_ok=True)
+
+            # Move the file
+            shutil.move(file, f"{save_folder}{file[len(src_folder):]}")
+
+    # Remove temporary folders
+    shutil.rmtree("data_temp", ignore_errors=True)
+
+
+def download_data_ruyters() -> None:
+    """
+    Downloads all images used by Ruyters.
+    """
+    # Download the parquet file containing all download links used by Ruyters
+    url = "https://raw.githubusercontent.com/wieswies/embedding_on_the_wall/2cf675b321fab25fabd71f0ddc0f1079c7fe73b3/f__nos-nu-analysis/datasets/input/full_df_clf_corrected.parquet"
+    response = requests.get(url)
+
+    # Create save folder if needed
+    if not os.path.exists("data/Embedding_On_The_Wall"):
+        os.makedirs("data/Embedding_On_The_Wall")
+
+    with open("data/Embedding_On_The_Wall/full_df_clf_corrected.parquet", "wb") as f:
+        f.write(response.content)
+    print("Download successful!")
+
+    # Download the images used by Ruyters
+    df = pd.read_parquet("data/Embedding_On_The_Wall/full_df_clf_corrected.parquet")
+    download_img_from_url("data/Embedding_On_The_Wall/full_images", df, "id", "img_link")
+
+
+def main_download_data() -> None:
+    """
+    Main runner for downloading all data needed for this project.
+    """
+    # Remove all data
+    print("Removing old data...")
+    shutil.rmtree("data", ignore_errors=True)
+
+    # Download the data used by Ruyters
+    print("\nDownloading data used by Ruyters...")
+    download_data_ruyters()
+
+    # Download the manually annotated news images
+    print("\nDownloading manually annotated news images...")
+    gdown.download_folder(
+        url="https://drive.google.com/drive/folders/1ErI645foO8fRMT2F46H0aDhFf6C14hRO?usp=drive_link",
+        output="data")
+
+    # Extract dat from zip file
+    with zipfile.ZipFile("data/Face detection.v1i.yolov8.zip") as z:
+        z.extractall("data/Face detection.v1i.yolov8")
+
+    # Remove zip file
+    os.remove("data/Face detection.v1i.yolov8.zip")
+
+    # Download the WIDER-face dataset
+    print("\nDownloading WIDER-face dataset...")
+    download_wider_face()
+
+    # Set kaggle datasets to download and their corresponding output path
     kaggle_datasets = [
+        "fareselmenshawii/face-detection-dataset",
         "vishesh1412/celebrity-face-image-dataset"
     ]
 
     output_paths = [
+        "data/Face-Detection-Dataset (Kaggle)",
         "data/celebrity-face-image-dataset"
     ]
 
+    # Download every specified dataset from kaggle
     for k, o in zip(kaggle_datasets, output_paths):
+        print(f"\nDownloading {k.split('/')[-1]}...")
         download_from_kaggle(k, o)
 
 
 if __name__ == '__main__':
     main_download_data()
-    # df = pd.read_parquet("data/Embedding_On_The_Wall/full_df_clf_corrected.parquet")
-    # download_img_from_url("data/Embedding_On_The_Wall/full_images", df, "id", "img_link")
-
-    # # Download latest version
-    # path = kagglehub.dataset_download("vishesh1412/celebrity-face-image-dataset")
-    # shutil.move(path, output_path)
-    # print("Path to dataset files:", path)
