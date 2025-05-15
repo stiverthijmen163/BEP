5c2ac0c009ef831c111a73dd3a805bdc50d8aff6
ThijmenB <thijmen.broeren@gmail.com>
2025-05-15 10:36:51 +0200

    clean app and index

--- visualization/app.py
+++ visualization/app.py
@@ -1,6 +1,8 @@
 from dash import Dash
 import dash_bootstrap_components as dbc
 
+
+# Set the necessary styles
 external_stylesheets = [
     {
         "href": "https://fonts.googleapis.com/css2?family=Roboto&display=swap",
@@ -10,9 +12,10 @@ external_stylesheets = [
         "rel": "stylesheet",
         "href": "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css",
     },
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",  # For icons
     dbc.themes.BOOTSTRAP
 ]
 
+# Initialize the app and server
 app = Dash(__name__, use_pages=True, external_stylesheets=external_stylesheets)
-server = app.server  # For deployment
+server = app.server
--- visualization/index.py
+++ visualization/index.py
@@ -2,69 +2,66 @@ from dash import html, dcc
 from app import app
 import dash
 
+
+# The initial layout of the app
 app.layout = html.Div(
-    style={
-        'backgroundColor': '#f0f8ff',
-        'minHeight': '100vh',
-        'margin': '0',
-        'padding': '0',
-        'boxSizing': 'border-box',
+    style={  # Set the background color for all pages
+        "backgroundColor": "#f0f8ff",
+        "minHeight": "100vh",
+        "margin": "0",
+        "padding": "0",
+        "boxSizing": "border-box",
     },
     children=[
-        dcc.Location(id='url'),  # Needed for navigation
+        dcc.Location(id="url"),  # Needed for navigation
 
         # Navigation bar
         html.Div(
             style={
-                'display': 'flex',
-                'justifyContent': 'center',
-                'gap': '5%',
-                'padding': '20px',
-                'backgroundColor': '#e0e0e0',
+                "display": "flex",
+                  "justifyContent": "center",
+                  "gap": "5%",
+                  "padding": "20px",
+                  "backgroundColor": "#e0e0e0"
             },
-            children=[
+            children=[  # Buttons to pages
                 dcc.Link(
-                    html.Button(
+                    html.Button(  # First page
                         "New Data",
                         style={
-                            'padding': '10px 20px',
-                            'fontSize': '16pt',
-                            'borderRadius': '12px',
-                            'border': 'none',
-                            'backgroundColor': '#2196F3',
-                            'color': 'white',
-                            'cursor': 'pointer',
-                            'width': "10vw"
-                        }
-                    ),
-                    href='/',
-                    refresh=False
-                ),
-                dcc.Link(
-                    html.Button(
-                        "Visualization",
-                        style={
-                            'padding': '10px 20px',
-                            'fontSize': '16pt',
-                            'borderRadius': '12px',
-                            'border': 'none',
-                            'backgroundColor': '#2196F3',
-                            'color': 'white',
-                            'cursor': 'pointer',
+                            "padding": "10px 20px",
+                            "fontSize": "16pt",
+                            "borderRadius": "12px",
+                            "border": "none",
+                            "backgroundColor": "#2196F3",
+                            "color": "white",
+                            "cursor": "pointer",
                             "width": "10vw"
                         }
                     ),
-                    href='/page2',
+                    href="/",
+                    refresh=False
+                ),
+                dcc.Link(
+                    html.Button(  # Second page
+                        "Visualization",
+                        style={
+                            "padding": "10px 20px",
+                            "fontSize": "16pt",
+                            "borderRadius": "12px",
+                            "border": "none",
+                            "backgroundColor": "#2196F3",
+                            "color": "white",
+                            "cursor": "pointer",
+                            "width": "10vw"
+                        }
+                    ),
+                    href="/page2",
                     refresh=False
                 )
             ]
         ),
-
         html.Div(
-            # style={
-            #     # 'padding': '40px',
-            #     'textAlign': 'center',
-            # },
             children=[
                 dash.page_container
             ]
@@ -74,4 +71,5 @@ app.layout = html.Div(
 
 
 if __name__ == "__main__":
+    # Start running the app
     app.run(debug=False)
